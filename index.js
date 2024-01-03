484c63669f9335f204e9fda16937f371ae9622e7
SoslyLW <45434295+SoslyLW@users.noreply.github.com>
2024-01-03 17:31:42 -0500

    Added graph showing history to bottom of page

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ $(document).ready(function(){
         return response.json();
     })
     .then(function (data) {
-        console.log(data);
         var teams = data.teams;
         let tableHolder = document.querySelector("#standings_table");
         let output = "";
@@ -27,7 +26,34 @@ $(document).ready(function(){
         tableHolder.innerHTML = output;
     })
 
-    // jQuery methods go here...
+    fetch("historyOutput.json")
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        var graph = data.graph;
 
-  
+        google.charts.load("current", { packages: ["corechart"] });
+        google.charts.setOnLoadCallback(drawChart);
+
+        function drawChart() {
+            var chartData = google.visualization.arrayToDataTable(graph);
+            var options = {
+                title: "Simulation History",
+                focusTarget: 'category',
+                legend: {
+                position: "bottom"
+                },
+                colors: ["#F47A38", "#5F259F", "#FFB81C", "#003087", "#D2001C", "#CE1126", "#000000", "#6F263D", "#002654", "#006847", "#CE1126", "#FF4C00", "#B9975B", "#A2AAAD", "#154734", "#AF1E2D", "#FFB81C", "#CE1126", "#F47D30", "#0038A8", "#000000", "#F74902", "#FCB514", "#006D75", "#99D9D9", "#002F87", "#002868", "#00205B", "#00843D", "#B4975A", "#C8102E", "#041E42"],
+                'chartArea': {'width': '90%'}
+            };
+            var chart = new google.visualization.LineChart(
+                document.getElementById("history_graph")
+            );
+
+            chart.draw(chartData, options);
+        }
+    })
+
+    // jQuery methods go here...
   });
