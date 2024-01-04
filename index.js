b2fdb7e1530ca5ff002bfff659002dd427a666e8
SoslyLW <45434295+SoslyLW@users.noreply.github.com>
2024-01-03 19:24:15 -0500

    Fixed issue with chart not resizing when window was resized

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ $(document).ready(function(){
         }
 
         tableHolder.innerHTML = output;
-    })
+    });
 
     fetch("historyOutput.json")
     .then(function (response) {
@@ -39,20 +39,21 @@ $(document).ready(function(){
         function drawChart() {
             var chartData = google.visualization.arrayToDataTable(graph);
             var options = {
-                title: "Simulation History",
+                chart: {
+                    title: "Simulation History",
+                },
                 focusTarget: 'category',
                 legend: 'none',
-                colors: ["#F47A38", "#5F259F", "#FFB81C", "#003087", "#D2001C", "#CE1126", "#000000", "#6F263D", "#002654", "#006847", "#CE1126", "#FF4C00", "#B9975B", "#A2AAAD", "#154734", "#AF1E2D", "#FFB81C", "#CE1126", "#F47D30", "#0038A8", "#000000", "#F74902", "#FCB514", "#006D75", "#99D9D9", "#002F87", "#002868", "#00205B", "#00843D", "#B4975A", "#C8102E", "#041E42"],
+                colors: ["#F47A38", "#5F259F", "#FFB81C", "#003087", "#D2001C", "#CE1126", "#000000", "#6F263D", "#002654", "#006847", "#CE1126", "#FF4C00", "#B9975B", "#A2AAAD", "#154734", "#AF1E2D", "#FFB81C", "#CE1126", "#F47D30", "#0038A8", "#000000", "#F74902", "#FCB514", "#006D75", "#99D9D9", "#002F87", "#002868", "#00205B", "#00843D", "#B4975A", "#C8102E", "#041E42"], //Array of colours for all 32 teams in alphabetical order
                 'chartArea': {'width': '80%'} //Not supported for material charts
             };
-            
-            console.log(window.innerWidth);
-            console.log(options);
 
+            // Create chart on load
             if (window.innerWidth >= 600) {
                 options.legend = {
                         position: "bottom",
-                    }
+                    };
+                options.chart.subtitle = "Click on a team in the legend to highlight their season history";
             // Mobile settings
             } else {
                 options.legend = {
@@ -65,8 +66,33 @@ $(document).ready(function(){
             );
 
             chart.draw(chartData, google.charts.Line.convertOptions(options));
+
+            // Function to handle redrawing chart whenever the window size changes
+            function resize () {
+                if (window.innerWidth >= 600) {
+                    options.legend = {
+                            position: "bottom",
+                        };
+                    // Legend is hidden on small screens so only give relevant instructions when legend is present
+                    options.chart.subtitle = "Click on a team in the legend to highlight their season history";
+                // Mobile settings
+                } else {
+                    options.legend = {
+                        position: "none"
+                    };
+                }
+
+                var chart = new google.charts.Line(
+                    document.getElementById("history_graph")
+                );
+                chart.draw(chartData, google.charts.Line.convertOptions(options));
+            }
+            
+            window.onload = resize;
+            window.onresize = resize;
         }
-    })
+        
+    });
 
     // jQuery methods go here...
   });
